8f532b003f07a241fe672fe87fbeecdf363b359d
Gerardo Duran-Martin <g.duran@me.com>
2022-12-11 09:06:25 +0000

    feat: smc.py

--- jsmc/samplers/sequential_monte_carlo.py
+++ jsmc/samplers/sequential_monte_carlo.py
@@ -10,50 +10,9 @@ class State:
     """
     State of the Sequential Importance Sampler
     """
-    particles: Float[Array, "num_steps dim_particle"]
-    log_weights: Float[Array, "num_steps"]
+    particles: Float[Array, "num_particles num_steps dim_particles"]
+    log_weights: Float[Array, "num_particles"]
     step: int = 0
-
-
-def step_accumulate_count(row_carry, row_target):
-    """
-    Accumulate a single observation
-    """
-    row_carry = row_carry + row_target 
-    return row_carry
-
-
-def step_accumulate_obs(row_carry, row_target, y_obs):
-    """
-    Accumulate a single observation
-    """
-    row_carry = row_carry + y_obs * row_target 
-    return row_carry
-
-
-def step_accumulate_sample(key, state_sis, row, proposal):
-    """
-    Accumulate the sample
-    """
-    carry_particles = state_sis.particles
-    sample_new = proposal.sample(key, state_sis)
-    carry_particles = carry_particles + sample_new * row
-    return carry_particles, sample_new
-
-
-def compound_elements(state: State, xs, proposal):
-    key, row, y_obs = xs
-    observations_new = step_accumulate_obs(state.observations, row, y_obs)
-    vcounter_new = step_accumulate_count(state.vcounter, row)
-    res_new = step_accumulate_sample(key, state, row, proposal)
-    particles_new, sample_new = res_new
-
-    state = state.replace(
-        particles=particles_new,
-        observations=observations_new,
-        vcounter=vcounter_new,
-    )
-    return state, sample_new
 
 
 def step_smc(state: State, xs, target, proposal):
@@ -72,46 +31,51 @@ def step_smc(state: State, xs, target, proposal):
 
 
 def _step_smc(
+    key,
     state: State,
-    xs,
     proposal: Callable,
     y: Float[Array, "num_steps dim_obs"],
 ):
     """
-    SMC step for a single particle
+    SMC step for a single particle.
+
+    In this case we assume that the proposal distribution
+    can consider samples from all previous and future steps.
+    It's up to the user to make sure that the proposal distribution
+    handles the inputs correctly.
+
+    TODO: We need a better way to handle the different
+          kinds of inputs to the proposal distribution
+          in size and number of arguments.
     """
-    key, row, y_obs = xs
-    y_obs = row * y_obs # Masking observation
     # 1. Resample
     ix_particle = jax.random.categorical(key, state.log_weights)
     resample_particle = state.particles[ix_particle]
     # 2. Propagate
-    # TODO: Add additional input to sample proposal
-    particle_new = proposal.sample(key, resample_particle, y, state.step)
+    # TODO: Add additional input to sample proposal.
+    particle_new = proposal.sample(key, resample_particle, state.step, y)
     # 3. Concatenate and update state
     particles_new = resample_particle.at[state.step].set(particle_new)
-    state_new = state.replace(particles=particles_new, step=state.step + 1)
-    return state_new
+    return particles_new
 
-
-def _init_state_single(_, num_steps, dim_particle):
-    """
-    Initialise single state of the particle SMC
-    """
-    state = State(
-        step=0,
-        particles=jnp.zeros((num_steps, dim_particle)),
-        log_weights=jnp.zeros(num_steps),
-    )
-    return state
 
 def _init_state(num_particles: int, num_steps: int, dim_particle: int):
     """
-    Initialise the state of the particle SMC
+    Initialise the state of the particle SMC. We assume that the
+    proposal distribution can consider samples from all previous
+    steps.
+
+    TODO: We wight want to be more smart on the size and handling
+          of the particles.
     """
-    dummyp = jnp.zeros(num_particles)
-    state = jax.vmap(_init_state_single, in_axes=(0, None, None))
-    state = state(dummyp, num_steps, dim_particle)
+    particles_init = jnp.zeros((num_particles, num_steps, dim_particle))
+    log_weights_init = jnp.zeros(num_particles)
+
+    state = State(
+        step=0,
+        particles=particles_init,
+        log_weights=log_weights_init,
+    )
     return state
 
 
